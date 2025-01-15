d728e0f340de0bb05d205a5df5223d913d1b2bfc
yshiraishiy <yuya0505soccer@gmail.com>
2025-01-15 15:35:32 +0900

    ✨ナビゲーションバーの実装

---
+++ src/main.js
@@ -0,0 +1,74 @@
+const products = [
+  {
+    name: "Sony Playstation 5",
+    url: "/playstation_5.png",
+    type: "games",
+    price: 499.99,
+  },
+  {
+    name: "Samsung Galaxy",
+    url: "/samsung_galaxy.png",
+    type: "smartphones",
+    price: 399.99,
+  },
+  {
+    name: "Cannon EOS Camera",
+    url: "/cannon_eos_camera.png",
+    type: "cameras",
+    price: 749.99,
+  },
+  {
+    name: "Sony A7 Camera",
+    url: "/sony_a7_camera.png",
+    type: "cameras",
+    price: 1999.99,
+  },
+  {
+    name: "LG TV",
+    url: "/lg_tv.png",
+    type: "televisions",
+    price: 799.99,
+  },
+  {
+    name: "Nintendo Switch",
+    url: "/nintendo_switch.png",
+    type: "games",
+    price: 299.99,
+  },
+  {
+    name: "Xbox Series X",
+    url: "/xbox_series_x.png",
+    type: "games",
+    price: 499.99,
+  },
+  {
+    name: "Samsung TV",
+    url: "/samsung_tv.png",
+    type: "televisions",
+    price: 1099.99,
+  },
+  {
+    name: "Google Pixel",
+    url: "/google_pixel.png",
+    type: "smartphones",
+    price: 499.99,
+  },
+  {
+    name: "Sony ZV1F Camera",
+    url: "/sony_zv1f_camera.png",
+    type: "cameras",
+    price: 799.99,
+  },
+  {
+    name: "Toshiba TV",
+    url: "/toshiba_tv.png",
+    type: "televisions",
+    price: 499.99,
+  },
+  {
+    name: "iPhone 14",
+    url: "/iphone_14.png",
+    type: "smartphones",
+    price: 999.99,
+  },
+];
